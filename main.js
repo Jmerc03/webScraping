766389ed3bdb6525851ceb7aa549555a51776b17
Jmerc03 <jaxmerc03@gmail.com>
2024-06-17 15:20:20 -0500

    List of link and data

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ async function scrapeSite(keyword) {
 
   const { data } = await axios.get(url);
   const $ = cheerio.load(data); // new addition
-  const imgs = [];
+  const lis = [];
   const results = [];
   $("div.preview").each((i, elem) => {
     const imgSrc = $(elem).find("img").attr("src");
@@ -14,16 +14,37 @@ async function scrapeSite(keyword) {
     // results.push({ imgSrc, text });
     results.push({ imgSrc });
   });
+  $("div.item-cataloged-data").each((i, elem) => {
+    const li = $(elem).find("li").text();
+    // const text = $(elem).find("span:first-child").text();
+    // results.push({ imgSrc, text });
+    lis.push({ li });
+  });
 
-  return results;
+  return { results, lis };
 }
 
 const keyword = "2014703221"; // change with any keyword you want
 
 scrapeSite(keyword)
   .then((result) => {
-    let thing = `${result[0].imgSrc}`;
-    thing = thing.slice(0, -10).concat("", "v.jpg");
-    console.log(thing);
+    let link = `${result.results[0].imgSrc}`;
+    link = link.slice(0, -10).concat("", "v.jpg");
+    console.log(link);
+
+    let data = `${result.lis[0].li}`;
+    const desc = data.split("\n");
+    const hope = noSpace(desc);
+    console.log(hope);
   })
   .catch((err) => console.log(err));
+
+function noSpace(array) {
+  const out = [];
+  for (let ele of array) {
+    if (ele.replace(/\s/g, "") !== "") {
+      out.push(ele);
+    }
+  }
+  return out;
+}
