fa6bdc28f8ee75a9004d98f7e40bcd7a01592504
Jmerc03 <jaxmerc03@gmail.com>
2024-06-18 09:03:52 -0500

    working Image object population

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ async function scrapeSite(keyword) {
     results.push({ imgSrc });
   });
   $("div.item-cataloged-data").each((i, elem) => {
-    const li = $(elem).find("li").text();
+    const li = $(elem).find("ul").text();
     // const text = $(elem).find("span:first-child").text();
     // results.push({ imgSrc, text });
     lis.push({ li });
@@ -23,21 +23,6 @@ async function scrapeSite(keyword) {
 
   return { results, lis };
 }
-
-const keyword = "2014703221"; // change with any keyword you want
-
-scrapeSite(keyword)
-  .then((result) => {
-    let link = `${result.results[0].imgSrc}`;
-    link = link.slice(0, -10).concat("", "v.jpg");
-    console.loggit(link);
-
-    let data = `${result.lis[0].li}`;
-    const desc = data.split("\n");
-    const hope = noSpace(desc);
-    console.log(hope);
-  })
-  .catch((err) => console.log(err));
 
 function noSpace(array) {
   const out = [];
@@ -56,3 +41,78 @@ function noSpace(array) {
   }
   return out;
 }
+
+//object constructor function
+function Image(LCCN) {
+  this.link = "";
+  this.title = "";
+  this.summary = "";
+  this.contrNames = "";
+  this.created = "";
+  this.heading = "";
+  this.genre = "";
+  this.notes = [];
+  this.medium = "";
+  this.callNum = "";
+  this.sourceColl = "";
+  this.repo = "";
+  this.digId = "";
+  this.lccn = LCCN;
+  this.reproductionNum = "";
+  this.rights = "";
+  this.format = "";
+  this.permalink = "";
+  this.metaDataFormats = [];
+  this.pop = function (arr) {
+    let i = 0;
+    const notesArr = [];
+    const metaArr = [];
+    for (let key in this) {
+      console.log(key, arr[i]);
+      if (arr[i].indexOf("-") === 0) {
+        while (arr[i].indexOf("-") === 0) {
+          notesArr.push(arr[i]);
+          i++;
+        }
+        // console.log("\n", notesArr, "\n");
+        this[key] = notesArr;
+      } else if (key === "metaDataFormats") {
+        while (i < arr.length) {
+          metaArr.push(arr[i]);
+          i++;
+        }
+        this[key] = metaArr;
+        break;
+      } else {
+        this[key] = arr[i];
+        i++;
+      }
+    }
+  };
+}
+
+//array of object creation
+
+const keyword = "2014703221"; // change with any keyword you want
+
+const num1 = new Image(keyword);
+console.log(num1);
+
+scrapeSite(keyword)
+  .then((result) => {
+    let link = `${result.results[0].imgSrc}`;
+    link = link.slice(0, -10).concat("", "v.jpg");
+    // console.log(link);
+
+    let data = `${result.lis[0].li}`;
+    // console.log(data);
+    const desc = data.split("\n");
+    // console.log(desc);
+    const hope = noSpace(desc);
+    // console.log(hope);
+
+    hope.unshift(link);
+    num1.pop(hope);
+    console.log(num1);
+  })
+  .catch((err) => console.log(err));
